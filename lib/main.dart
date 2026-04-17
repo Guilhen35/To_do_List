a1340d0e3d667bbe64b41278982ac02d4ee6cd79
Ph <seuemail@example.com>
2026-04-17 11:46:54 -0300

    feat: primeiro commit - todo list flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'todo_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(
+              seedColor:
+                  Colors.deepPurple,
+            ),
+      ),
+      home: const TodoListPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
